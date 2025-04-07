f0bd9028fe07a4c9fb17d934202c344df26d5f36
MsMauve <zoetropesilver@gmail.com>
2025-04-06 23:36:14 -0600

    add number button and clear button functionality + css styling

--- calculator.js
+++ calculator.js
@@ -1,19 +1,50 @@
-function add (a, b) {
+const add = (a, b) => {
     return a + b;
 }
 
-function subtract(a, b) {
+const subtract = (a, b) => {
     return a - b;
 }
 
-function multiply(a, b) {
+const multiply = (a, b) => {
     return a * b;
 }
 
-function divide(a, b) {
+const divide = (a, b) => {
     return a / b;
 }
 
 let numA;
 let numB;
 let operator;
+
+const display = document.querySelector("#display");
+const allDigitBtn = document.querySelectorAll(".num-btn");
+const clearBtn = document.querySelector("#ac-btn");
+
+const updateDisplay = (num) => {
+    if (display.textContent === '0') {
+        display.textContent = num;
+    } else if (display.textContent.length < 12) {
+        display.textContent += num;
+    }
+    else {
+        alert("Sorry, it's a mini calculator!");
+    }
+};
+
+const clearDisplay = () => {
+    display.textContent = 0;
+}
+
+allDigitBtn.forEach((btn) => {
+    btn.addEventListener('click', function() {
+        updateDisplay(btn.textContent)
+    });
+})
+
+clearBtn.addEventListener('click', function() {
+    clearDisplay();
+});
+
+
