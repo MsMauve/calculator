5062deaef84dc612c3c99c2624e60cdc050a954c
MsMauve <zoetropesilver@gmail.com>
2025-04-08 10:14:08 -0600

    add operator functionality

--- calculator.js
+++ calculator.js
@@ -21,12 +21,21 @@ let operator;
 const display = document.querySelector("#display");
 const allDigitBtn = document.querySelectorAll(".num-btn");
 const clearBtn = document.querySelector("#ac-btn");
+const displayNum = display.textContent;
+const equalsBtn = document.querySelector("#equals-btn");
+const absBtn = document.querySelector("#abs-btn");
+const percentBtn = document.querySelector("#percent-btn");
+const divideBtn = document.querySelector("#division-btn");
+const multiplyBtn = document.querySelector("#multiply-btn")
+const subtractBtn = document.querySelector("#subtract-btn");
+const addBtn = document.querySelector("#add-btn");
+const decimalBtn = document.querySelector("#decimal-btn");
 
 const updateDisplay = (num) => {
-    if (display.textContent === '0') {
-        display.textContent = num;
-    } else if (display.textContent.length < 8) {
-        display.textContent += num;
+    if (displayNum === '0') {
+        displayNum = num;
+    } else if (displayNum.length < 6) {
+        displayNum += num;
     }
     else {
         alert("Sorry, it's a mini calculator!");
@@ -34,7 +43,7 @@ const updateDisplay = (num) => {
 };
 
 const clearDisplay = () => {
-    display.textContent = 0;
+    displayNum = 0;
 }
 
 allDigitBtn.forEach((btn) => {
@@ -47,4 +56,54 @@ clearBtn.addEventListener('click', function() {
     clearDisplay();
 });
 
+divideBtn.addEventListener('click', function() {
+    numA = displayNum;
+    operator = "divide";
+    clearDisplay();
+});
 
+multiplyBtn.addEventListener('click', function() {
+    numA = displayNum;
+    operator = "multiply";
+    clearDisplay();
+});
+
+subtractBtn.addEventListener('click', function() {
+    numA = displayNum;
+    operator = "subtract";
+    clearDisplay();
+});
+
+addBtn.addEventListener('click', function() {
+    numA = displayNum;
+    operator = "add";
+    clearDisplay();
+});
+
+equalsBtn.addEventListener('click',() => {
+    if (Number(displayNum) && numA) {
+        numB = displayNum;
+        switch (operator) {
+            case "divide":
+                displayNum = divide(numA, numB);
+                numA = null;
+                numB = null;
+                break;
+            case "multiply":
+                displayNum = multiply(numA, numB);
+                numA = null;
+                numB = null;
+                break;
+            case "subtract":
+                displayNum = subtract(numA, numB);
+                numA = null;
+                numB = null;
+                break;
+            case "add":
+                displayNum = add(numA, numB);
+                numA = null;
+                numB = null;
+                break;
+        }
+    }
+});
